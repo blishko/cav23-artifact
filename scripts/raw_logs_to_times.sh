8b20ce701e2420d43b4e0ce3912b1b886067a5fa
Martin Blicha <martin.blicha@gmail.com>
2023-04-25 19:40:21 +0200

    Expand scripts to automatically extract time files

---
+++ scripts/raw_logs_to_times.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+extract_script="${SCRIPTS_ROOT}/extract_times.sh"
+log_dir="${ARTIFACT_ROOT}/raw_logs"
+times_dir="${ARTIFACT_ROOT}/times"
+
+category=$1
+
+top_dir=${log_dir}/${category}
+
+if [ ! -d ${top_dir} ]; then
+    echo "Error, directory with raw logs does not exist"
+    exit 1
+fi
+echo "Processing benchmark directory ${category}"
+mkdir -p ${times_dir}/${category}
+for tool_dir in ${top_dir}/*; do
+    tool_name=`basename ${tool_dir}`
+    echo "Processing tool ${tool_name}"
+    bash ${extract_script} ${tool_dir} >  ${times_dir}/${category}/${tool_name}.csv 
+done
--- scripts/run_extra_small_lia.sh
+++ scripts/run_extra_small_lia.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 timelimit=300
 
 while getopts "t:" o; do
@@ -17,3 +19,5 @@ ${script_dir}/run_golem.sh ${benchmark_dir} $timelimit spacer
 ${script_dir}/run_golem.sh ${benchmark_dir} $timelimit lawi
 ${script_dir}/run_golem.sh ${benchmark_dir} $timelimit split-tpa
 ${script_dir}/run_eldarica.sh ${benchmark_dir} $timelimit
+
+${script_dir}/raw_logs_to_times.sh extra-small-lia
