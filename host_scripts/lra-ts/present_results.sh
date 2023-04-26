edb2c483cadcb17c954c884e826afff5baa07fd5
Martin Blicha <martin.blicha@gmail.com>
2023-04-26 15:55:26 +0200

    Fix script to present results to work properly with relative paths

--- host_scripts/lra-ts/present_results.sh
+++ host_scripts/lra-ts/present_results.sh
@@ -6,10 +6,13 @@ if [[ $# == 0 ]]; then
     exit 1;
 fi
 
-CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
+script_dir=$(cd "$(dirname "$0")" && pwd)
 
 timelimit=300
-results_dir=$1
+
+#results_dir=$1
+# resolve possibly relative path to absolute path
+results_dir="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 shift
 
 while getopts "t:" o; do
@@ -21,19 +24,14 @@ while getopts "t:" o; do
 done
 
 
-#echo ${CURRENT_DIR}
-ORIGINAL_DIR=$(pwd)
-cd ${CURRENT_DIR}
-
 #recompute the virtual best
 vb_file="${results_dir}/VB.csv"
 [ ! -e ${vb_file} ] || rm ${vb_file}    # delete virtual best file if exists
-bash recompute_virtual_best.sh ${results_dir} -t ${timelimit} > ${vb_file}
+bash ${script_dir}/recompute_virtual_best.sh ${results_dir} -t ${timelimit} > ${vb_file}
 
 # compute the summary of the results (outputs to standard output)
-bash to_summary.sh ${results_dir}
+bash ${script_dir}/to_summary.sh ${results_dir}
 
 # compute the plot in (computes .ps file in this script's directory)
-bash to_gnu_cactus_plot_sat.sh ${results_dir}
+bash ${script_dir}/to_gnu_cactus_plot_sat.sh ${results_dir}
 
-cd ${ORIGINAL_DIR}
